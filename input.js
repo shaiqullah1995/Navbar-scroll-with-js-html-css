fdc8a14747fa03b7eeb39678ef54b25d3b691ac1
Shafiqullah Khaliqyaar <shafiqullahkhaliqyaar@gmail.com>
2023-05-08 18:00:17 +0530

    first commit

---
+++ input.js
@@ -0,0 +1,32 @@
+// console.log('this a test page');
+// let ByBtn = document.getElementById("ByBtn");
+// let content = document.getElementById("content");
+
+// function getData(){
+//     console.log("Started getData")
+//      url ="harry.text"
+//     function(url).then ((response) =>{
+//         console.log("Inside Fist then")
+//         return response .text();
+//     }).then((data)=>{
+//         console.log("Iside Scoend then")
+//         console.log(data)
+//     })
+
+//     //Its for post API
+
+// function postData(){
+//     url = "https://foodbukka.herokuapp.com/api/v1/menu"
+//     data ='{"name": "shafi233","age":"25","salary":"55555"}';
+
+// body:data }
+// fetch(url,params).then(response=> response.json())
+// .then(data => console.log(data))
+
+// }
+
+// console.log("Befor runing getDtat")
+// getData()
+// console.log("After runing getData")
+// postData()
+
